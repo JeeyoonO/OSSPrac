37ff11a35e12f5eea46d0783c3e2f00d1763b894
JeeyoonO <eriaw29@dgu.ac.kr>
2026-04-03 15:39:12 +0900

    Complete ossprac1 code

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-# 이름과 학번 입력 받기
 name = input("이름을 입력하세요: ")
 student_id = input("학번을 입력하세요: ")
+dept = input("학과를 입력하세요: ")
+univ = input("학교를 입력하세요: ")
 
-# 결과 출력
 print("\n<출력>")
 print(f"이름: {name}")
 print(f"학번: {student_id}")
+print(f"학과: {dept}")
+print(f"학교: {univ}")
